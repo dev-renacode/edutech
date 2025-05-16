f3b857180e9e4b7635e7173930fc2ed56088161b
Renato Herrera <re.herrerac@duocuc.cl>
2025-05-16 18:09:25 -0400

    CursoService creado

--- src/main/java/com/example/edutech/Controller/CursoController.java
+++ src/main/java/com/example/edutech/Controller/CursoController.java
@@ -1,5 +1,11 @@
 package com.example.edutech.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/cursos")
 public class CursoController {
-    
+
 }
