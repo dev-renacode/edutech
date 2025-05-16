f16553b12f53f622eb495eae0c24f44746784b06
ViXx07 <vicen.perezb@duocuc.cl>
2025-05-16 18:03:49 -0400

    Creación de los atributos del curso.java en Model

--- src/main/java/com/example/edutech/Model/Curso.java
+++ src/main/java/com/example/edutech/Model/Curso.java
@@ -1,5 +1,18 @@
 package com.example.edutech.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Curso {
-    
+    private int id;
+    private String nombreCurso;
+    private int fechaCreacion;
+    private String docenteCurso;
+    private String descripcion;
+    private int horasDuracion;    
 }
