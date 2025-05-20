3d4390b55b853eb7630fa126dfad907bfa9c92e2
Felipe Ardiles <fe.ardiles@duocuc.cl>
2025-05-20 15:46:57 -0400

    Creación - CarritoController.java

---
+++ src/main/java/com/example/edutech/Controller/CarritoController.java
@@ -0,0 +1,63 @@
+package com.example.edutech.Controller;
+
+import com.example.edutech.Model.Curso;
+import com.example.edutech.Service.CursoService;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api/v1/carrito") //Cambiar por si acaso
+public class CarritoController {
+    private final List<Curso> carrito = new ArrayList<>();
+    
+    @Autowired
+    private CursoService cursoService;
+    
+    //Método para agregar un libro al carrito de compras.
+    @PostMapping("/agregar/{id}")
+    public String agregarCurso(@PathVariable int id) {
+        Curso curso = cursoService.getCursoid(id);
+        if(curso != null){
+            carrito.add(curso);
+            return "El curso se agregó al carrito: " + curso.getNombreCurso(); 
+        }
+
+        return "El curso no fue encontrado.";
+    }
+    
+    @DeleteMapping("/eliminar/{id}")
+    public String eliminarCurso(@PathVariable int id){
+        boolean eliminado = carrito.removeIf(curso -> curso.getId() == id);
+        return eliminado ? "El curso ha sido eliminado." : "Curso no encontrado.";
+    }
+
+    @GetMapping
+    public List<Curso> verCarrito(@RequestParam String param) {
+        return carrito;
+    }
+
+    @DeleteMapping("/vaciar")
+    public String vaciarCarrito() {
+        carrito.clear();
+        return "El carrito ha sido vaciado.";
+    }
+    
+    @GetMapping("/total")
+    public int totalCursosCarrito() {
+        return carrito.size();
+    }
+
+
+}
+
