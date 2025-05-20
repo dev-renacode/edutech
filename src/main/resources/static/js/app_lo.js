1ae2d32b0e7bc0cd7fb85d741e0366867eeecccf
Felipe Ardiles <fe.ardiles@duocuc.cl>
2025-05-20 15:53:46 -0400

    Creación - js del login

---
+++ src/main/resources/static/js/app_lo.js
@@ -0,0 +1,22 @@
+let API_URL = "http://localhost:8080/api/v1/usuarios/login";
+
+const login = () => {
+    fetch(API_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.result === "OK"){
+            sessionStorage.setItem("nombreUsuario", data.nombre)
+            window.location.href = "/index.html";
+        }else{
+            alert("Acceso ha sido denegado")
+        }
+    })
+}
+
